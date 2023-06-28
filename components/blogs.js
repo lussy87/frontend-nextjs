1ce142b79f47598a033cfc21abffafa33f0d8ca4
lussy87 <lussyika@gmail.com>
2023-06-28 10:26:58 +0700

    fourth commit

--- components/blogs.js
+++ components/blogs.js
@@ -42,7 +42,7 @@ const Blogs  = () => {
             <br />
             <div className="text-left">
             <button
-                    className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full inline-block text-[17px]"
+                    className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full inline-block text-[17px]"
                     onClick={() => handleDetailClick(blog.id)}
                   >
                     Detail
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import Container from '../../components/container';
 import Navbar from '../../components/navbar';
 
+
 const BlogDetail = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -57,7 +58,17 @@ const BlogDetail = () => {
 
     <Container>
       <div className="flex flex-col gap-10">
-        <h1 className="text-3xl font-bold">Detail Blog</h1>
+     
+      <div className="flex items-center mb-4">
+      <a href="/">
+          <a className="flex items-center text-gray-500 dark:text-gray-300 hover:text-indigo-500">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+            <h1 className="text-3xl font-bold">Detail Blog</h1>
+          </a>
+        </a>
+      </div>
         {blog && users.length > 0 ? (
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <h2><strong>Title:</strong> {blog.title}</h2>
